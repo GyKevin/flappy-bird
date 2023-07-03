9211b12f2fb45b657048c3b0d8f6e8e133a6e40d
Kevin <kvindso@gmail.com>
2023-07-03 11:56:31 +0200

    added pipes
    pipes toegevoegd, ik heb ze laten bewegen naar links en de top pipe heeft een random generated height. de bottom pipe moet ik nog toevoegen

--- flappybird.js
+++ flappybird.js
@@ -19,6 +19,20 @@ let bird ={
     height: birdHeight,
 }
 
+//pipe width and height
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+//pipe image
+let pipeImg;
+let bottompipeImg;
+
+//physics engine
+let velocityX = -2; //pipes moving left
+
 
 // onload change the board size to the boardWidth and boardHeight
 window.onload = function() {
@@ -32,6 +46,51 @@ window.onload = function() {
     birdImg = new Image();
     birdImg.src = "./img/flappybird.png";
     birdImg.onload = function() {
-    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
+
+    //load the pipe image
+    pipeImg = new Image();
+    pipeImg.src = "./img/toppipe.png";
+
+    bottompipeImg = new Image();
+    bottompipeImg.src = "./img/bottompipe.png";
+
+    requestAnimationFrame(update);
+    // place pipes every 1000 ms
+    setInterval(placepipe, 1000);
 };
+
+//update the board and redraw bird image
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, boardWidth, boardHeight);
+    
+    //draw the bird inside the frame
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    //geef de pipe een velocity zodat het naar links beweegt
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.Img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+
+}
+
+// place the pipes
+function placepipe() {
+
+    //place random pipes based on pipeHeight
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random() *(pipeHeight/2);
+
+    let topPipe = {
+        Img: pipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false,
+    }
+    pipeArray.push(topPipe);
+}
