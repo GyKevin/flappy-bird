5db26ab78dc73c1f1a93f20f32244b7569fa5f44
Kevin <kvindso@gmail.com>
2023-07-03 13:11:04 +0200

    collision detection, bird movement, game over en scorebord toegevoegd
    collision detection is compleet door github copilot gemaakt, als de vogel de grond of de pipes aanraakt dan is het game over. Elke keer als de vogel een pipe passt, krijg je 1 punt

--- flappybird.js
+++ flappybird.js
@@ -31,8 +31,14 @@ let pipeImg;
 let bottompipeImg;
 
 //physics engine
-let velocityX = -2; //pipes moving left
+let velocityX = -2; //make the pipes move left
+let velocityY = 0; //make the bird fall down or jump up
+let gravity = 0.08; //give the bird gravity so it falls down
 
+// game over
+let gameOver = false;
+//score
+let score = 0;
 
 // onload change the board size to the boardWidth and boardHeight
 window.onload = function() {
@@ -59,30 +65,68 @@ window.onload = function() {
     requestAnimationFrame(update);
     // place pipes every 1000 ms
     setInterval(placepipe, 1000);
+
+    // add event listener for keydown
+    document.addEventListener("keydown", moveBird);
 };
 
-//update the board and redraw bird image
+//update the board and redraw bird image everytime the frame updates
 function update() {
     requestAnimationFrame(update);
+    // if game over, stop painting the canvas
+    if (gameOver) {
+        return;
+    }
     context.clearRect(0, 0, boardWidth, boardHeight);
     
+    // add gravity to the bird by retracting the gravity from the velocity
+    velocityY += gravity;
+    // add velocity to the bird everytime it jumps and make the limit 0 so it doesnt go out of the canvas
+    bird.y = Math.max(bird.y + velocityY, 0);
     //draw the bird inside the frame
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    // if the bird hits the ground, game over
+    if (bird.y + bird.height >= boardHeight) {
+        gameOver = true;
+    }
 
     //geef de pipe een velocity zodat het naar links beweegt
     for (let i = 0; i < pipeArray.length; i++) {
         let pipe = pipeArray[i];
         pipe.x += velocityX;
         context.drawImage(pipe.Img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+        // check if the bird hits the pipes, if it does, game over
+        if (checkCollision(bird, pipe)) {
+            gameOver = true;
+        }
     }
 
+    // if the bird passes the pipe, add 1 to the score
+    context.fillStyle = "white";
+    context.font = "24px Verdana";
+    context.fillText("Score: " + score, 10, 30);
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        if (bird.x > pipe.x + pipe.width && !pipe.passed) {
+            score+= 0.5;
+            pipe.passed = true;
+        }
+    }
 }
 
 // place the pipes
 function placepipe() {
+    // if game over, stop placing pipes
+    if (gameOver) {
+        return;
+    }
 
     //place random pipes based on pipeHeight
     let randomPipeY = pipeY - pipeHeight/4 - Math.random() *(pipeHeight/2);
+    // space between the pipes (this should make it so that the space is allways the same size)
+    let openingspace = board.height/5;
 
     let topPipe = {
         Img: pipeImg,
@@ -93,4 +137,32 @@ function placepipe() {
         passed: false,
     }
     pipeArray.push(topPipe);
+
+    let bottomPipe = {
+        Img: bottompipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingspace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false,
+    }
+    pipeArray.push(bottomPipe);
+}
+
+// if the mousebutton is pressed, set the velocity to -6 which makes the bird go up
+// - velocityY makes the bird go up and + velocityY makes the bird go down
+function moveBird(e) {
+    if (e.code == "Space") {
+        // make the bird jump
+        velocityY = -4;
+    }
+}
+
+// check if the bird hits the pipes
+// this whole function is made by github copilot lmao
+function checkCollision(a,b) {
+    return a.x < b.x + b.width &&
+              a.x + a.width > b.x &&
+                a.y < b.y + b.height &&
+                    a.y + a.height > b.y;
 }
